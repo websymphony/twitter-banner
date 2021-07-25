5413966c07fc7dfe254e91299a97724d374e6f31
Amit Gaur <amitgaur.web@gmail.com>
2021-07-25 17:52:09 +1000

    Fixes utm source

--- pages/index.js
+++ pages/index.js
@@ -242,11 +242,11 @@ export default function Home() {
             </svg>
               Follow me on twitter
             </a>
-            <a className="flex flex-col items-center mx-4" target="_blank" rel="noopener nofollow" title="Websymphony site" href="https://websymphony.net?utm_source=paraphrasing-tool">
+            <a className="flex flex-col items-center mx-4" target="_blank" rel="noopener nofollow" title="Websymphony site" href="https://websymphony.net?utm_source=twitter-banner">
               <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
               https://websymphony.net
             </a>
-            <a className="flex flex-col items-center mx-4" target="_blank" rel="noopener nofollow" title="hipCV" href="https://hipcv.com?utm_source=paraphrasing-tool">
+            <a className="flex flex-col items-center mx-4" target="_blank" rel="noopener nofollow" title="hipCV" href="https://hipcv.com?utm_source=twitter-banner">
               <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
               Create free resumes
             </a>
